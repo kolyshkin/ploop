kolyshkin/ploop

commit c80141418bbc9a71e1a53bddb8506eb6b2e830d4

Rework check_snapshot_mount() v2
This commit tries to make check_snapshot_mount() easier.
The motivation behind the change is it was used incorrectly
from do_delete_snapshot().

In do_delete_snapshot() we only want to check one snapshot
and we don't care about the parent one. So, let's simplify
it to only check one snapshot.

In ploop_merge_snapshot_by_guid() we want to check
two snapshots -- the parent one and the child one. So,
let's just call new check_snapshot_mount() twice -- for
the parent and for the child.

While at it:
1. Change the check_snapshot_mount() to avoid excessive code nesting
2. Move it (and get_devs_str()) to ploop.c as it is used from different places.
3. Improve the error message about busy snapshot -- add snapshot UUID.

[v2: minor code readability improvements]

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.c
@@ -3677,3 +3677,57 @@ out:
 	ploop_close_dd(di);
 	return ret;
 }
+
+static const char *get_devs_str(char **devs, char *buf, int size)
+{
+	char **p;
+	char *sp = buf;
+	char *ep = buf + size;
+
+	for (p = devs; *p != NULL; p++) {
+		sp += snprintf(sp, ep - sp, "%s ", *p);
+		if (sp >= ep)
+			break;
+	}
+	return buf;
+}
+
+int check_snapshot_mount(struct ploop_disk_images_data *di,
+		const char *guid, const char *fname, int temp)
+{
+	int ret = 0;
+	char **devs, **p;
+	char buf[512];
+
+	ret = ploop_get_dev_by_delta(di->images[0]->file,
+			fname, NULL, &devs);
+	if (ret == 1)
+		return 0;
+	if (ret == -1)
+		return SYSEXIT_SYS;
+
+	/* There is one or more ploop devices found */
+	if (!temp) {
+		ploop_err(0, "Snapshot %s is busy by device(s): %s", guid,
+				get_devs_str(devs, buf, sizeof(buf)));
+		ret = SYSEXIT_EBUSY;
+		goto out;
+	}
+
+	/* Try to unmount temp snapshot(s) */
+	for (p = devs; *p != NULL; p++) {
+		ret = ploop_umount(*p, NULL);
+		if (ret) {
+			/* print current device and remaining ones */
+			ploop_err(0, "Snapshot %s is busy by device(s): %s", guid,
+					get_devs_str(p, buf, sizeof(buf)));
+			goto out;
+		}
+	}
+
+	ret = 0;
+out:
+	ploop_free_array(devs);
+
+	return ret;
+}

lib/merge.c
@@ -683,63 +683,6 @@ merge_done2:
 	return ret;
 }
 
-static const char *get_devs_str(char **devs, char *buf, int size)
-{
-	char **p;
-	char *sp = buf;
-	char *ep = buf + size;
-
-	for (p = devs; *p != NULL; p++) {
-		sp += snprintf(sp, ep - sp, "%s ", *p);
-		if (sp >= ep)
-			break;
-	}
-	return buf;
-}
-
-int check_snapshot_mount(struct ploop_disk_images_data *di,
-		int temporary, const char *parent_fname,
-		const char *child_fname, const char *child_guid)
-{
-	int ret = 0;
-	char **devs, **p;
-	char buf[512];
-
-	/* Check if upper (child) delta that will be merged and
-	 * destroyed is mounted
-	 */
-	if (guidcmp(child_guid, di->top_guid) != 0 &&
-			ploop_get_dev_by_delta(di->images[0]->file,
-				child_fname, NULL, &devs) == 0)
-	{
-		ploop_err(0, "Snapshot is busy by device(s): %s",
-				get_devs_str(devs, buf, sizeof(buf)));
-		ret = SYSEXIT_EBUSY;
-		goto out;
-	}
-
-	/* check if snapshot (parent) delta is mounted */
-	if (ploop_get_dev_by_delta(di->images[0]->file,
-			parent_fname, NULL, &devs) == 0)
-	{
-		/* unmount temporary snapshot */
-		if (temporary) {
-			for (p = devs; *p != NULL; p++) {
-				ret = ploop_umount(*p, NULL);
-				if (ret)
-					break;
-			}
-		} else {
-			ploop_err(0, "Snapshot is busy by device(s): %s",
-					get_devs_str(devs, buf, sizeof(buf)));
-			ret = SYSEXIT_EBUSY;
-		}
-	}
-out:
-	ploop_free_array(devs);
-	return ret;
-}
-
 int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di,
 		const char *guid, int merge_mode, const char *new_delta)
 {
@@ -759,8 +702,7 @@ int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di,
 	int merge_top = 0;
 	int raw = 0;
 	int online = 0;
-	int snap_idx;
-	int temporary = 0;
+	int parent_idx, child_idx; /* parent and child snapshot ids */
 	struct merge_info info = {};
 	int i, nelem;
 
@@ -796,14 +738,20 @@ int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di,
 				parent_guid);
 		goto err;
 	}
-	snap_idx = find_snapshot_by_guid(di, parent_guid);
-	if (snap_idx == -1) {
+	parent_idx = find_snapshot_by_guid(di, parent_guid);
+	if (parent_idx == -1) {
 		ploop_err(0, "Can't find snapshot by uuid %s",
 				parent_guid);
 
 		goto err;
 	}
-	temporary = di->snapshots[snap_idx]->temporary;
+	child_idx = find_snapshot_by_guid(di, child_guid);
+	if (child_idx == -1) {
+		ploop_err(0, "Can't find snapshot by uuid %s",
+				child_guid);
+
+		goto err;
+	}
 
 	nelem = ploop_get_child_count_by_uuid(di, parent_guid);
 	if (nelem > 1) {
@@ -868,7 +816,7 @@ int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di,
 		end_level = 1;
 		/* Only base image could be in RAW format */
 		if (di->mode == PLOOP_RAW_MODE &&
-				!guidcmp(di->snapshots[snap_idx]->parent_guid, NONE_UUID))
+				!guidcmp(di->snapshots[parent_idx]->parent_guid, NONE_UUID))
 			raw = 1;
 	}
 
@@ -884,8 +832,12 @@ int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di,
 	}
 	names[2] = NULL;
 
-	ret = check_snapshot_mount(di, temporary, parent_fname,
-			child_fname, child_guid);
+	ret = check_snapshot_mount(di, parent_guid, parent_fname,
+			di->snapshots[parent_idx]->temporary);
+	if (ret)
+		goto err;
+	ret = check_snapshot_mount(di, child_guid, child_fname,
+			di->snapshots[child_idx]->temporary);
 	if (ret)
 		goto err;
 
@@ -965,7 +917,8 @@ int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di,
 
 	if (ret == 0)
 		ploop_log(0, "ploop %s %s has been successfully merged",
-				get_snap_str(temporary), parent_guid);
+				get_snap_str(di->snapshots[parent_idx]->temporary),
+				parent_guid);
 
 err:
 	for (i = 0; names[i] != NULL; i++)

lib/ploop.h
@@ -424,8 +424,7 @@ PL_EXT int ploop_change_fmt_version(struct ploop_disk_images_data *di,
 int ploop_get_dev_by_delta(const char *delta, const char *topdelta,
 		const char *component_name, char **out[]);
 int check_snapshot_mount(struct ploop_disk_images_data *di,
-		int temporary, const char *parent_fname,
-		const char *child_fname, const char *child_guid);
+		const char *guid, const char *fname, int temp);
 int create_image(const char *file, __u32 blocksize, off_t size_sec, int mode,
 		int version);
 int do_replace_delta(int devfd, int level, int imgfd, __u32 blocksize,

lib/snapshot.c
@@ -102,15 +102,7 @@ static int do_delete_snapshot(struct ploop_disk_images_data *di, const char *gui
 			return SYSEXIT_PARAM;
 		}
 
-		char *parent_fname = find_image_by_guid(di, snap->parent_guid);
-		if (parent_fname == NULL) {
-			ploop_err(0, "Unable to find image by uuid %s",
-					snap->parent_guid);
-			return SYSEXIT_PARAM;
-		}
-
-		ret = check_snapshot_mount(di, snap->temporary, parent_fname,
-				fname, guid);
+		ret = check_snapshot_mount(di, guid, fname, snap->temporary);
 		if (ret)
 			return ret;
 

commit 1db21a832bace04979c50b8d9fd8918d60834e44

check_snapshot_mount: don't check top delta
This is a regression introduced by commit c801414 -- since that one
we can't merge top delta online because we find it's busy.

The solution is to not check the top delta, as it was before the
commit c801414.

https://bugzilla.openvz.org/3215

Reported-by: Andrey Tataranovich <tataranovich@gmail.com>
Reported-by: Roman Gakhov <roman.gakhov@gmail.com>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.c
@@ -3709,6 +3709,10 @@ int check_snapshot_mount(struct ploop_disk_images_data *di,
 	char **devs, **p;
 	char buf[512];
 
+	/* Don't check top delta */
+	if (guidcmp(guid, di->top_guid) == 0)
+		return 0;
+
 	ret = ploop_get_dev_by_delta(di->images[0]->file,
 			fname, NULL, &devs);
 	if (ret == 1)