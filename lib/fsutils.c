kolyshkin/ploop

commit 1c6e10af945193983aeac12ea16303e7e8246a78

run_prg_rc(): add HIDE_NZ_EXIT flag
In case hide_mask has the flag HIDE_NZ_EXIT set, do not print
an error message telling that program exited with non-zero status.

To be used by the next commit.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.h
@@ -409,6 +409,7 @@ int is_valid_blocksize(__u32 blocksize);
 int run_prg(char *const argv[]);
 #define HIDE_STDOUT	1 << 0	/* hide process' stdout */
 #define HIDE_STDERR	1 << 1	/* hide process' stderr */
+#define HIDE_NZ_EXIT	1 << 2	/* hide error about non-zero exit code */
 int run_prg_rc(char *const argv[], int hide_mask, int *rc);
 int p_memalign(void **memptr, size_t alignment, size_t size);
 PL_EXT int guidcmp(const char *p1, const char *p2);

lib/util.c
@@ -320,7 +320,8 @@ int run_prg_rc(char *const argv[], int hide_mask, int *rc)
 		}
 		if (ret == 0)
 			return 0;
-		ploop_err(0, "Command %s exited with code %d", cmd, ret);
+		if (! (hide_mask & HIDE_NZ_EXIT))
+			ploop_err(0, "Command %s exited with code %d", cmd, ret);
 	} else if (WIFSIGNALED(status)) {
 		ploop_err(0, "Command %s received signal %d",
 				cmd, WTERMSIG(status));

commit b7d4766cef9cbee1c98e07dcc3f2f5a23ce4fee8

make_fs(): simplify mkfs retry logic
Commit 1c6e10a introduced a new flag for run_prg_rc() to hide an error
about non-zero exit code. The same effect can be easily achieved b
asking for the program exit code (i.e. if the last argument
of run_prg_rc() is not NULL).

So, this is a revert of 1c6e10a and an appropriate change in logic.

This is a followup to commit d496ba8.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/fsutils.c
@@ -144,10 +144,12 @@ int make_fs(const char *device, const char *fstype, unsigned int fsblocksize,
 	 * visible, so if there's any error it will be shown during
 	 * the second run.
 	 */
-	int hide = HIDE_STDERR | HIDE_NZ_EXIT;
+	int hide = HIDE_STDERR;
 	int retry = 1;
+	int rc;
+
 run:
-	if (run_prg_rc(argv, hide, NULL) != 0) {
+	if (run_prg_rc(argv, hide, &rc) != 0 || rc != 0) {
 		if (!retry)
 			return SYSEXIT_MKFS;
 

lib/ploop.h
@@ -409,7 +409,6 @@ int is_valid_blocksize(__u32 blocksize);
 int run_prg(char *const argv[]);
 #define HIDE_STDOUT	1 << 0	/* hide process' stdout */
 #define HIDE_STDERR	1 << 1	/* hide process' stderr */
-#define HIDE_NZ_EXIT	1 << 2	/* hide error about non-zero exit code */
 int run_prg_rc(char *const argv[], int hide_mask, int *rc);
 int p_memalign(void **memptr, size_t alignment, size_t size);
 PL_EXT int guidcmp(const char *p1, const char *p2);

lib/util.c
@@ -320,8 +320,7 @@ int run_prg_rc(char *const argv[], int hide_mask, int *rc)
 		}
 		if (ret == 0)
 			return 0;
-		if (! (hide_mask & HIDE_NZ_EXIT))
-			ploop_err(0, "Command %s exited with code %d", cmd, ret);
+		ploop_err(0, "Command %s exited with code %d", cmd, ret);
 	} else if (WIFSIGNALED(status)) {
 		ploop_err(0, "Command %s received signal %d",
 				cmd, WTERMSIG(status));